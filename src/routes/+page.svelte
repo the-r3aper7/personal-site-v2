c0060611659fbed8db73c2e593e3073750aba156
the-r3aper7 <saisrikardumpeti@gmail.com>
2023-10-03 16:58:25 +0530

    added open graph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,12 @@
 
 <Hero />
 <MySkills />
+
+<svelte:head>
+	<title>Sai Srikar Dumpeti</title>
+	<meta name="description" content="Personal Website of Sai Srikar Dumpeti" />
+	<meta name="og:title" content="Sai Srikar Dumpeti" />
+	<meta name="og:image" content="https://images.pexels.com/photos/15843151/pexels-photo-15843151.jpeg?w=640&h=427" />
+	<meta name="og:type" content="website" />
+	<meta name="og:url" content="https://ssd7.app" />
+</svelte:head>
